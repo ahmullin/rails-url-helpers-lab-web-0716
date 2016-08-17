17887ded707a1a7df107fd5353e1884af0fd8f29
Alison <alisonhmullin@gmail.com>
2016-08-17 15:53:16 -0400

    complete URL helpers lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
+require 'pry'
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student.change_status
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,20 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def get_status
+    if self.active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
+
+  def change_status
+    self.active = !self.active
+    self.save
+  end
+
 end
